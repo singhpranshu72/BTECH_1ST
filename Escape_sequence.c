6ec048affa0469307f448c9a15a15190979497ac
singhpranshu99 <pranshu.singh@gla.ac.in>
2026-09-17 10:16:51 +0530

    Create Escape_sequence.c

---
+++ Escape_sequence.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{ 
+
+printf("Hello \n \t escape sequences \\Doc\'s\\MyPrograms world");
+//  escape sequences in C programming language
+// \n is a newline character
+// \t is a tab character
+// \\ is a backslash
+// \' is a single quote
+// \" is a double quote
+
+} 
